06c8ce0c7467209833b28dcd92f380684598fe02
BRosencrans <brandon.rosencrans@gmail.com>
2023-01-10 12:54:22 -0800

    added javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//api key for openweather
+var APIkey = "9695d701e522f2ef687386464a455171"
+
+//document variables
+
+var pastCitys = document.querySelector("#prev-citys")
+var cityInput = document.querySelector("city-input")
+var search = document.querySelector("#search-btn")
+var todaysWeather = document.querySelector("#todays-weather")
+var city = document.querySelector("#city")
+var theDate = document.querySelector("#the-date")
+var temp = document.querySelector("#temp")
+var wind = document.querySelector("#wind")
+var humidity = document.querySelector("#humidity")
+
+
